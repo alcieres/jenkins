7be82d6cfda7a60f4ae947619b93e11b85091fcd
Alcieres <alcieres@gmail.com>
2018-06-04 20:06:31 -0300

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+/* HelloWorld.java
+
+ */
+
+
+
+public class hello
+
+{
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+
+} 
+/* O Alcieres passou por arqui e o Frabrício esteve depois */
